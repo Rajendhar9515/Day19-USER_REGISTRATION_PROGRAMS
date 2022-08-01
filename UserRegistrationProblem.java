1f87889d6a06f3e431baff16950bc94e7a2e5f5b
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-08-01 19:17:25 +0530

    Validated E-Mail using Regex.

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -9,10 +9,10 @@ public class UserRegistrationProblem {
 		System.out.println("\t WELCOME TO USER REGISTRATION PROBLEM \n");
 
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter First Name");
-		String first_name = input.nextLine();
 
 		// UC-1 :- As a User need to enter a valid First Name.
+		System.out.println("Enter First Name");
+		String first_name = input.nextLine();
 		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", first_name);
 		System.out.println(b);
 
@@ -21,6 +21,12 @@ public class UserRegistrationProblem {
 		String last_name = input.nextLine();
 		boolean b1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", last_name);
 		System.out.println(b1);
+
+		// UC-3:- As a User need to enter a valid email.
+		System.out.println("Enter Email");
+		String email = input.nextLine();
+		boolean b2 = Pattern.matches("^(.+)@(\\S+)$", email);
+		System.out.println(b2);
 
 	}
 
