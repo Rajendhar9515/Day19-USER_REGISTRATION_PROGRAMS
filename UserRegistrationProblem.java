a5f048b753bb84a25770fe87b1ed209d1b9171bd
Rajendhar9515 <108645584+Rajendhar9515@users.noreply.github.com>
2022-08-02 20:16:43 +0530

    Update UserRegistrationProblem.java

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -4,29 +4,39 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-	public static void main(String[] args) {
+	static Scanner input = new Scanner(System.in);
 
-		System.out.println("\t WELCOME TO USER REGISTRATION PROBLEM \n");
-
-		Scanner input = new Scanner(System.in);
-
-		// UC-1 :- As a User need to enter a valid First Name.
+	// UC-1 :- As a User need to enter a valid First Name.
+	public static void firstNameValidation() {
 		System.out.println("Enter First Name");
-		String first_name = input.nextLine();
-		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", first_name);
+		String firstName = input.nextLine();
+		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName);
 		System.out.println(b);
+	}
 
-		// UC-2 :- As a User need to enter a valid Last Name.
+	// UC-2 :- As a User need to enter a valid Last Name.
+	public static void lastNameValidation() {
 		System.out.println("Enter last Name");
-		String last_name = input.nextLine();
-		boolean b1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", last_name);
+		String lastName = input.nextLine();
+		boolean b1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName);
 		System.out.println(b1);
+	}
 
-		// UC-3:- As a User need to enter a valid email.
+	// UC-3:- As a User need to enter a valid email.
+	public static void emailValidation() {
 		System.out.println("Enter Email");
 		String email = input.nextLine();
 		boolean b2 = Pattern.matches("^(.+)@(\\S+)$", email);
 		System.out.println(b2);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("\t WELCOME TO USER REGISTRATION PROBLEM \n");
+
+		firstNameValidation();
+		lastNameValidation();
+		emailValidation();
 
 	}
 
