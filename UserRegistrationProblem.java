10e1eed0102e593637c20db7868d7560761ce7b0
Rajendhar9515 <108645584+Rajendhar9515@users.noreply.github.com>
2022-08-02 20:20:11 +0530

    Update UserRegistrationProblem.java

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -4,36 +4,48 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
+	static Scanner input = new Scanner(System.in);
+
+	// UC-1 :- As a User need to enter a valid First Name.
+	public static void firstNameValidation() {
+		System.out.println("Enter First Name");
+		String firstName = input.nextLine();
+		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName);
+		System.out.println(b);
+	}
+
+	// UC-2 :- As a User need to enter a valid Last Name.
+	public static void lastNameValidation() {
+		System.out.println("Enter last Name");
+		String lastName = input.nextLine();
+		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName);
+		System.out.println(b);
+	}
+
+	// UC-3:- As a User need to enter a valid email.
+	public static void emailValidation() {
+		System.out.println("Enter Email");
+		String email = input.nextLine();
+		boolean b = Pattern.matches("^(.+)@(\\S+)$", email);
+		System.out.println(b);
+	}
+
+	// UC-4:- As a User need to follow predefined Mobile Format.
+	public static void mobileNumberValidation() {
+		System.out.println("Enter Mobile Number");
+		String num = input.nextLine();
+		boolean b = Pattern.matches("^[0-9]{2}[0-9]{10}", num);
+		System.out.println(b);
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("\t WELCOME TO USER REGISTRATION PROBLEM \n");
 
-		Scanner input = new Scanner(System.in);
-
-		// UC-1 :- As a User need to enter a valid First Name.
-		System.out.println("Enter First Name");
-		String first_name = input.nextLine();
-		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", first_name);
-		System.out.println(b);
-
-		// UC-2 :- As a User need to enter a valid Last Name.
-		System.out.println("Enter last Name");
-		String last_name = input.nextLine();
-		boolean b1 = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", last_name);
-		System.out.println(b1);
-
-		// UC-3:- As a User need to enter a valid email.
-		System.out.println("Enter Email");
-		String email = input.nextLine();
-		boolean b2 = Pattern.matches("^(.+)@(\\S+)$", email);
-		System.out.println(b2);
-
-		// UC-4:- As a User need to follow predefined Mobile Format.
-		System.out.println("Enter Mobile Number");
-		String num = input.nextLine();
-		boolean b3 = Pattern.matches("^[0-9]{2}[0-9]{10}", num);
-		System.out.println(b3);
+		firstNameValidation();
+		lastNameValidation();
+		emailValidation();
+		mobileNumberValidation();
 
 	}
-
 }
